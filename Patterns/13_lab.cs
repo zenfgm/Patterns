db8679bf2ff691710577a0c5302033cc8854b1ea
zenfgm <166837566+zenfgm@users.noreply.github.com>
2024-12-09 11:28:55 +0500

    Add files via upload

---
+++ Patterns/13_lab.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LogisticsSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> stockItems = new List<string> { "Товар1", "Товар2", "Товар3" };
+            List<string> orderItems = new List<string> { "Товар1", "Товар4" };
+
+            bool isOrderValid = CheckStock(stockItems, orderItems);
+
+            if (isOrderValid)
+            {
+                Console.WriteLine("Все товары в наличии. Заказ подтвержден.");
+            }
+            else
+            {
+                Console.WriteLine("Некоторые товары недоступны. Отправлено уведомление клиенту.");
+            }
+        }
+
+        static bool CheckStock(List<string> stockItems, List<string> orderItems)
+        {
+            foreach (var item in orderItems)
+            {
+                if (!stockItems.Contains(item))
+                {
+                    Console.WriteLine($"Товар {item} недоступен.");
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
